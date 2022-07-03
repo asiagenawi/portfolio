d2d0f2598e08b2b10f0664a2f94b9ab8bb88f40a
ASIA GENAWI <asiagenawi@gmail.com>
2022-07-03 14:48:10 -0400

    7/3

---
+++ projects.js
@@ -0,0 +1,30 @@
+
+  $(document).ready(function () {
+$(".button1").click(function() {
+   alert("Sorry, try again!");
+
+
+   });
+});
+
+$(document).ready(function () {
+   $(".button2").click(function() {
+      alert("Sorry, try again!");
+   
+   
+      });
+   });
+   
+
+   
+  $(document).ready(function () {
+   $(".button3").click(function() {
+      alert("Congrats, you're correct!");
+   
+   
+      });
+   });
+   
+   
+
+
